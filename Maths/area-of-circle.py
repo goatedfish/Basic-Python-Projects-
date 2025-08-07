450a252a1855122e30531a562d809b405a5e32a1
Ayush <ayush438500@gmail.com>
2025-08-07 16:52:29 +0530

    first file

---
+++ Maths/area-of-circle.py
@@ -0,0 +1,9 @@
+import math
+
+r = int(input("Enter the radius of the circle: "))
+
+pie = math.pi
+
+result = pie * r * r
+
+print(result)
---
+++ README.md
@@ -0,0 +1,1 @@
+Here I'll be puting my very basic python projects
